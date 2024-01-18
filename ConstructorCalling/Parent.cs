4536c306e1176349da1cc019f9a9ce49b8872c0c
Sumit Kumar <703201313@GENPACT.COM>
2024-01-18 22:26:54 +0530

    added ebooks

---
+++ ConstructorCalling/Parent.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorCalling
+{
+    internal class Parent
+    {
+        public Parent(string parameter)
+        {
+            Console.WriteLine($"Writing from normal parameterized constructor of {nameof(Parent)}");
+        }
+        static Parent()
+        {
+            Console.WriteLine($"Writing from static constructor of {nameof(Parent)}");
+        }
+        public Parent() {
+            Console.WriteLine($"Writing from normal constructor of {nameof(Parent)}");
+        }
+    }
+
+    internal class Child : Parent
+    {
+        public Child(string parameter):base(parameter)
+        {
+            Console.WriteLine($"Writing from normal parameterized constructor of {nameof(Child)}");
+        }
+        static Child()
+        {
+            Console.WriteLine($"Writing from static constructor of {nameof(Child)}");
+        }
+        public Child()
+        {
+            Console.WriteLine($"Writing from normal constructor of {nameof(Child)}");
+        }
+    }
+}
---
+++ ConstructorCalling/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ConstructorCalling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            var obj= new Child("this");
+
+            Console.WriteLine("Calling completed!");
+            Console.ReadLine();
+
+            /* Below is the result of calling. It can be seen the order of calling is
+             * 1. Static of Child
+             * 2. Static of Parent
+             * 3. Normal of Parent
+             * 4. Normal of Child
+
+            /*
+             * Hello, World!
+             * Writing from static constructor of Child
+             * Writing from static constructor of Parent
+             * Writing from normal parameterized constructor of Parent
+             * Writing from normal parameterized constructor of Child
+             * Calling completed!
+             */
+        }
+    }
+}
